bdddcae593ec514b887dce52a8d3e353e6c8b169
Delroy Brown <delroybrown.db@gmail.com>
2021-04-01 22:45:40 +0100

    NueUi Admin

---
+++ NueUiBackend/NueUiBackend/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from wagtail.admin import urls as wagtailadmin_urls
+from wagtail.core import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
+
+urlpatterns = [
+    path('business-admin/', admin.site.urls),
+    path('site-admin/', include(wagtailadmin_urls)),
+    path('documents/', include(wagtaildocs_urls)),
+    path('pages/', include(wagtail_urls)),
+    re_path(r'', include(wagtail_urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
